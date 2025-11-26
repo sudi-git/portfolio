2ea3cf8c128830c7441fd4b9d1e48e79cc1c62c9
Sudikshya Bhattarai <sudikshya.bhattarai@tuni.fi>
2025-11-26 17:27:59 +0200

    feat: added all the components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-import { Button } from "@/components/ui/button";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Projects from "./components/projects";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hi!! Sudikshya here
-          </h1>
-          <Button>click me</Button>
-        </div>
-      </main>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   );
 }
